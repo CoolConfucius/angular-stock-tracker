61bf0652598c927bde80c7ae7a9c3a112b08ce8b
Benjamin Huang <coolconfucius@yahoo.com>
2016-02-09 21:01:54 -0800

    implemented ngStorage

--- main.js
+++ main.js
@@ -1,30 +1,45 @@
 'use strict';
 
-var app = angular.module('routingTest', ['ui.router']); 
+var app = angular.module('routingTest', ['ui.router', 'ngStorage']); 
 
-app.config(function($stateProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $urlRouterProvider, $localStorageProvider) {
   $stateProvider
-    .state('home', {url: '/', templateUrl: './partials/home.html' })
+    .state('home', {url: '/', templateUrl: './partials/home.html', controller: 'homeCtrl' })
     .state('add', {url: '/add', templateUrl: './partials/add.html', controller: 'addCtrl' })
     .state('list', {url: '/list', templateUrl: './partials/list.html', controller: 'listCtrl' })
   
   $urlRouterProvider.otherwise('/'); 
 });
 
-app.controller('listCtrl', function($scope, $state, Stock) {
-  $scope.stocks = Stock.list; 
+app.controller('homeCtrl', function($scope, $state, $localStorage, Stock) {
+  // $scope.$storage = $localStorage; 
+  // if (!$scope.$storage.stocks || !$scope.$storage.symbols) {
+  //   $scope.$storage = $localStorage.$default({
+  //     symbols: [],
+  //     stocks: []
+  //   });
+  // };
 
-  $scope.removeStock = function(symbol) {
-    Stock.remove(symbol)
-  }
-
+  // Stock.symbols = $scope.$storage.symbols; 
+  // Stock.list = $scope.$storage.stocks; 
 });
 
-app.controller('addCtrl', function($scope, $http, Stock){
+app.controller('listCtrl', function($scope, $state, $localStorage, Stock) {
+  $scope.stocks = Stock.list; 
+  $scope.removeStock = function(symbol) {
+    Stock.remove(symbol);
+    // $scope.$storage.symbols = Stock.symbols;
+    // $scope.$storage.stocks = Stock.list;
+  }
+});
+
+app.controller('addCtrl', function($scope, $http, $localStorage, Stock){
   $scope.addStock = function() {
     var cleanSymbol = $scope.newSymbol.toUpperCase().replace(/\W/, '')
     $scope.newSymbol = ''; 
     Stock.addSymbol(cleanSymbol); 
+    // $scope.$storage.symbols = Stock.symbols;
+    // $scope.$storage.stocks = Stock.list;
   }
 
   $scope.search = function() {
@@ -32,15 +47,27 @@ app.controller('addCtrl', function($scope, $http, Stock){
     $scope.lookResults = Stock.lookResults; 
   }
 
-  $scope.quickAdd = function(symbol) {    
+  $scope.quickAdd = function(symbol, $localStorage) {    
     Stock.addSymbol(symbol); 
+    // $scope.$storage.symbols = Stock.symbols;
+    // $scope.$storage.stocks = Stock.list;
   }
 
 });
 
-app.service('Stock', function($http){
-  this.list = []; 
-  this.symbols = []; 
+app.service('Stock', function($http, $localStorage){
+  
+  if (!$localStorage.stocks || !$localStorage.symbols) {
+    $localStorage.$default({
+      symbols: [],
+      stocks: []
+    });
+  };
+
+
+
+  this.list = $localStorage.stocks; 
+  this.symbols = $localStorage.symbols; 
   this.lookResults = []; 
 
   this.addSymbol = function(symbol) {
@@ -55,6 +82,8 @@ app.service('Stock', function($http){
     $http.jsonp(apiSymbol + symbol + jsonp)
     .then(res => {      
       this.list.push(res.data);
+      $localStorage.stocks = this.list; 
+      $localStorage.symbols = this.symbols; 
       swal(symbol + " successfully added!", "success")
     }, function(res) {
       console.log('error:', res);
@@ -65,6 +94,8 @@ app.service('Stock', function($http){
     var index = this.symbols.indexOf(symbol);
     this.symbols.splice(index, 1);
     this.list.splice(index, 1);
+    $localStorage.stocks = this.list; 
+    $localStorage.symbols = this.symbols; 
     swal(symbol + " successfully removed!", "success")
   };
 
